ba356ffab1b27dc98c2c3fd263dae14dbe313b25
OriolCS2 <36154523+OriolCS2@users.noreply.github.com>
2020-01-03 21:22:38 +0100

    menu and player doing

--- BattleSimulator/BattleSimulator/BattleSimulator.cpp
+++ BattleSimulator/BattleSimulator/BattleSimulator.cpp
@@ -6,6 +6,72 @@ enum State {
 	EXIT,
 	INIT,
 };
+State state = INIT;
+
+class Character {
+
+public:
+	Character(int basic_attack, int defense, int live, int mana, int magic_attack, int magic_defense, int speed, const char* name)
+		: basic_attack(basic_attack), defense(defense), live(live), mana(mana), magic_attack(magic_attack), 
+		magic_defense(magic_defense), speed(speed), name(name) {}
+
+	bool IsAlive() {
+		return live > 0;
+	}
+	static Character* CompareSpeed(Character* c1, Character* c2) {
+		return (c1->speed >= c2->speed) ? c1 : c2;
+	}
+	const char* GetName() {
+		return name.data();
+	}
+	void PrintStats() {
+		printf(" ---------------%s Stats---------------\n", name.data());
+		printf(" ---------------%s Stats---------------\n\n", name.data());
+	}
+
+private:
+
+	int basic_attack = 0;
+	int defense = 0;
+	int live = 0;
+	int mana = 0;
+	int magic_attack = 0;
+	int magic_defense = 0;
+	int speed = 0;
+
+	std::string name;
+};
+
+void BattleIA()
+{
+
+}
+
+void BattlePlayer()
+{
+	system("cls");
+	printf("-----------------------------------------------------------------------\n");
+	printf("----------------------------Player vs IA-------------------------------\n");
+	printf("-----------------------------------------------------------------------\n\n");
+
+	Character player(10, 5, 100, 60, 8, 4, 10, "Player");
+	Character enemy(10, 5, 100, 60, 8, 4, 8, "Enemy");
+	Character* active = Character::CompareSpeed(&player, &enemy);
+
+	int round = 1;
+
+	player.PrintStats();
+	enemy.PrintStats();
+	
+	while (player.IsAlive() && enemy.IsAlive()) {
+		char intro_input = getchar();
+		printf("ROUND NUMEBER %i\n", round);
+		printf("%s turn\n", active->GetName());
+	}
+
+
+	// TOOD: return to main menu
+}
 
 int main()
 {
@@ -19,17 +85,16 @@ int main()
 	printf(" 2- Player vs IA (2)\n");
 	printf(" 3- Exit (3)\n");
 
-	State state = INIT;
-
 	while (state != EXIT) {
 		char intro_input = getchar();
-
 		switch (intro_input) {
 		case '1': {
 			state = IA;
+			BattleIA();
 			break; }
 		case '2': {
 			state = PLAYER;
+			BattlePlayer();
 			break; }
 		case '3': {
 			state = EXIT;
@@ -43,6 +108,5 @@ int main()
 			printf(" 3- Exit (3)\n");
 			break; }
 		}
-
 	}
 }
