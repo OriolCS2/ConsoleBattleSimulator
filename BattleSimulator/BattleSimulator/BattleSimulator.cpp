604b2a71dc5995a91562132677db664de95065ec
OriolCS2 <36154523+OriolCS2@users.noreply.github.com>
2020-01-04 01:48:56 +0100

    now is playable

--- BattleSimulator/BattleSimulator/BattleSimulator.cpp
+++ BattleSimulator/BattleSimulator/BattleSimulator.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "BattleSimulator.h"
+#include <time.h>
 
 enum State {
 	IA,
@@ -38,16 +38,195 @@ public:
 	bool CanDoBuff() {
 		return mana >= cost_mana_buff;
 	}
+	void DoSpecialAttack(Character* against) {
+		mana -= cost_mana_special_attack;
+		if (time_buff_attack != 0) {
+			against->DecreaseHealth(special_attack + buff_attack);
+		}
+		else {
+			against->DecreaseHealth(special_attack);
+		}
+		if (against->IsAlive()) {
+			printf(" %s was hitted with a special attack and has %i health\n", against->name.data(), against->GetHealth());
+		}
+	}
+	void DoBasicAttack(Character* against) {
+		if (time_buff_attack != 0) {
+			against->DecreaseHealth(basic_attack + buff_attack);
+		}
+		else {
+			against->DecreaseHealth(basic_attack);
+		}
+		if (against->IsAlive()) {
+			printf(" %s was hitted with a basic attack and has %i health\n", against->name.data(), against->GetHealth());
+		}
+	}
+	void DecreaseHealth(int damage) {
+		if (time_buff_defense != 0) {
+			if (damage > defense + buff_defense) {
+				live -= abs((defense + buff_defense) - damage);
+			}
+		}
+		else {
+			if (damage > defense) {
+				live -= abs(defense - damage);
+			}
+		}
+	}
+	int GetHealth() {
+		return live;
+	}
+	int GetMana() {
+		return mana;
+	}
+	void BuffAttack() {
+		time_buff_attack = buff_duration;
+		printf(" %s Attack and special attack increased by %i points for %i rounds\n", name.data(), Character::buff_attack, Character::buff_duration - 1);
+	}
+	void BuffDefense() {
+		time_buff_defense = buff_duration;
+		printf(" %s Defense increased by %i points for %i rounds\n", name.data(), Character::buff_defense, Character::buff_duration - 1);
+	}
+	void CheckBuffs() {
+		if (time_buff_attack != 0) {
+			--time_buff_attack;
+			if (time_buff_attack == 0) {
+				printf(" %s Attack has return to the normal state\n", name.data());
+			}
+		}
+		if (time_buff_defense != 0) {
+			--time_buff_defense;
+			if (time_buff_defense == 0) {
+				printf(" %s Defense has return to the normal state\n", name.data());
+			}
+		}
+	}
+	void DoRandomAttack(Character* against) {
+		bool do_buff_attack = false;
+		bool do_buff_defense = false;
+		bool basic = false;
+		bool special = false;
+		srand(time(NULL));
+		if (time_buff_attack != 0 && time_buff_defense != 0) { // 2 buffs
+			int r = rand() % 2;
+			switch (r) {
+			case 0: {
+				basic = true;
+				break; }
+			case 1: {
+				if (CanDoSpecialAttack()) {
+					special = true;
+				}
+				else {
+					basic = true;
+				}
+				break; }
+			}
+		}
+		else if (time_buff_attack != 0 && time_buff_defense == 0) { // attack buff
+			int r = rand() % 3;
+			switch (r) {
+			case 0: {
+				basic = true;
+				break; }
+			case 1: {
+				if (CanDoSpecialAttack()) {
+					special = true;
+				}
+				else {
+					basic = true;
+				}
+				break; }
+			case 2: {
+				if (CanDoBuff()) {
+					do_buff_defense = true;
+				}
+				else {
+					basic = true;
+				}
+				break; }
+			}
+		}
+		else if (time_buff_attack == 0 && time_buff_defense != 0) { // defense buff
+			int r = rand() % 3;
+			switch (r) {
+			case 0: {
+				basic = true;
+				break; }
+			case 1: {
+				if (CanDoSpecialAttack()) {
+					special = true;
+				}
+				else {
+					basic = true;
+				}
+				break; }
+			case 2: {
+				if (CanDoBuff()) {
+					do_buff_attack = true;
+				}
+				else {
+					basic = true;
+				}
+				break; }
+			}
+		}
+		else { // no buff
+			int r = rand() % 4;
+			switch (r) {
+			case 0: {
+				basic = true;
+				break; }
+			case 1: {
+				if (CanDoSpecialAttack()) {
+					special = true;
+				}
+				else {
+					basic = true;
+				}
+				break; }
+			case 2: {
+				if (CanDoBuff()) {
+					do_buff_attack = true;
+				}
+				else {
+					basic = true;
+				}
+				break; }
+			case 3: {
+				if (CanDoBuff()) {
+					do_buff_defense = true;
+				}
+				else {
+					basic = true;
+				}
+				break; }
+			}
+		}
+
+		if (basic) {
+			DoBasicAttack(against);
+		}
+		else if (special) {
+			DoSpecialAttack(against);
+		}
+		else if (do_buff_attack) {
+			BuffAttack();
+		}
+		else if (do_buff_defense) {
+			BuffDefense();
+		}
+	}
 
 	void PrintStats() {
 		printf("---------------%s Stats---------------\n", name.data());
 		printf("---------------HEALTH: %i\n", live);
 		printf("---------------MANA: %i\n", mana);
-		printf("---------------BASIC ATTACK DAMAGE: %i\n", basic_attack);
-		printf("---------------SPECIAL ATTACK DAMAGE: %i\n", special_attack);
-		printf("---------------DEFENSE: %i\n", defense);
+		printf("---------------BASIC ATTACK DAMAGE: %i\n", (time_buff_attack == 0) ? basic_attack : basic_attack + buff_attack);
+		printf("---------------SPECIAL ATTACK DAMAGE: %i\n", (time_buff_attack == 0) ? special_attack : special_attack + buff_attack);
+		printf("---------------DEFENSE: %i\n", (time_buff_defense == 0) ? defense : defense + buff_defense);
 		printf("---------------SPEED: %i\n", speed);
-		printf("---------------%s Stats---------------\n\n", name.data());
+		printf("---------------%s Stats---------------\n", name.data());
 	}
 
 private:
@@ -63,6 +242,15 @@ private:
 	int cost_mana_buff = 0;
 
 	std::string name;
+
+	int time_buff_attack = 0;
+	int time_buff_defense = 0;
+
+public:
+
+	static const int buff_attack = 5;
+	static const int buff_defense = 4;
+	static const int buff_duration = 4; // the turn you buff counts here too
 };
 
 void BattleIA()
@@ -70,17 +258,12 @@ void BattleIA()
 
 }
 
-void PrintPlayerIATitle()
-{
-	printf("-----------------------------------------------------------------------\n");
-	printf("----------------------------Player vs IA-------------------------------\n");
-	printf("-----------------------------------------------------------------------\n\n");
-}
-
 void BattlePlayer()
 {
 	system("cls");
-	PrintPlayerIATitle();
+	printf("-----------------------------------------------------------------------\n");
+	printf("----------------------------Player vs IA-------------------------------\n");
+	printf("-----------------------------------------------------------------------\n\n");
 
 	Character player(10, 4, 100, 60, 20, 10, "Player", 15, 10);
 	Character enemy(10, 4, 100, 60, 20, 10, "Enemy", 15, 10);
@@ -89,65 +272,118 @@ void BattlePlayer()
 	int round = 1;
 
 	player.PrintStats();
+	printf("\n");
 	enemy.PrintStats();
-	
+	printf("\n");
+	printf(" Press Enter to start the battle\n");
+	getchar();
+	getchar();
+	system("cls");
+
+	int turn = 0;
+	bool round_finished = true;
 	while (player.IsAlive() && enemy.IsAlive()) {
-		printf("----------------------------ROUND NUMEBER %i----------------------------\n", round);
+		if (round_finished) {
+			printf("----------------------------ROUND NUMEBER %i----------------------------\n", round);
+			round_finished = false;
+		}
 		printf("---------------%s turn---------------\n\n", active->GetName());
 
 		if (active == &player) {
+			++turn;
+			printf("--------Current Health: %i\n", player.GetHealth());
+			printf("--------Current Mana: %i\n", player.GetMana());
+			printf("--------Enemy Health: %i\n\n", enemy.GetHealth());
+
 			printf(" Choose your action:\n");
 			printf("	A) Basic Attack (a)\n");
 			printf("	B) Special Attack (%i mana) (b)\n", active->CostSpecialAttack());
 			printf("	C) Buff Attack (%i mana) (c)\n", active->CostBuff());
 			printf("	D) Buff Defense (%i mana) (d)\n", active->CostBuff());
+			printf("	E) See Player Stats (e)\n");
+			printf("	F) See Enemy Stats (f)\n");
 
 			bool next_turn = false;
 			while (!next_turn) {
 				char action = getchar();
+				printf("\n");
 				switch (action)
 				{
 				case 'a': {
-
+					active->DoBasicAttack(&enemy);
+					next_turn = true;
+					active = &enemy;
 					break; }
 				case 'b': {
 					if (active->CanDoSpecialAttack()) {
-
+						active->DoSpecialAttack(&enemy);
+						next_turn = true;
+						active = &enemy;
 					}
 					else {
-						printf("You don't have enough mana\n");
+						printf(" You don't have enough mana\n");
 					}
 					break; }
 				case 'c': {
 					if (active->CanDoBuff()) {
-
+						active->BuffAttack();
+						next_turn = true;
+						active = &enemy;
 					}
 					else {
-						printf("You don't have enough mana\n");
+						printf(" You don't have enough mana\n");
 					}
 					break; }
 				case 'd': {
 					if (active->CanDoBuff()) {
-
+						active->BuffDefense();
+						next_turn = true;
+						active = &enemy;
 					}
 					else {
-						printf("You don't have enough mana\n");
+						printf(" You don't have enough mana\n");
 					}
+					break; }
+				case 'e': {
+					player.PrintStats();
+					break; }
+				case 'f': {
+					enemy.PrintStats();
 					break; }
 				case '\n': {
 					break; }
 				default: {
-					printf("Choose a correct action\n");
+					printf(" Choose a correct action\n");
 					break; }
 				}
 			}
+			player.CheckBuffs();
+			printf("\n----------------------------------------\n");
+			if (enemy.IsAlive()) {
+				printf("\n Press Enter to finish turn\n");
+				getchar();
+				getchar();
+			}
 		}
 		else {
-
+			++turn;
+			active->DoRandomAttack(&player);
+			enemy.CheckBuffs();
+			printf("\n----------------------------------------\n");
+			if (player.IsAlive()) {
+				printf("\n Press Enter to start your turn\n");
+				getchar();
+			}
+			active = &player;
+		}
+		if (turn == 2) {
+			++round;
+			turn = 0;
+			round_finished = true;
 		}
 	}
-
-
+	printf("WIN!!");
+	getchar();
 	// TOOD: return to main menu
 }
 
@@ -162,7 +398,7 @@ int main()
 	printf(" 1- IA vs IA (1)\n");
 	printf(" 2- Player vs IA (2)\n");
 	printf(" 3- Exit (3)\n");
-
+	printf("\n");
 	while (state != EXIT) {
 		char intro_input = getchar();
 		switch (intro_input) {
@@ -184,6 +420,7 @@ int main()
 			printf(" 1- IA vs IA (1)\n");
 			printf(" 2- Player vs IA (2)\n");
 			printf(" 3- Exit (3)\n");
+			printf("\n");
 			break; }
 		}
 	}
